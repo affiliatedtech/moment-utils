48d82dcbed0ef971c8c41919244a17dc061dbdd7
Daniel Lindegren <onosendi@dlindegren.com>
2022-05-09 06:15:18 -0700

    [Refactor] change func/var names to snake_case

--- moment-utils.js
+++ moment-utils.js
@@ -1,30 +1,32 @@
+'use strict';
+
 const moment = require('moment');
 
-function getBaseHour(dt) {
-  const dateTime = moment(dt);
-  const minutesToSeconds = dateTime.minutes() * 60;
-  const seconds = dateTime.seconds();
-  const totalSeconds = minutesToSeconds + seconds;
-  const baseHour = dateTime.subtract(totalSeconds, 'seconds');
+function get_base_hour(dt) {
+    const date_time = moment(dt);
+    const minutes_to_seconds = date_time.minutes() * 60;
+    const seconds = date_time.seconds();
+    const total_seconds = minutes_to_seconds + seconds;
+    const base_hour = date_time.subtract(total_seconds, 'seconds');
 
-  return baseHour;
+    return base_hour;
 }
 
-function getTimeDiff(start, end, fnString = 'asSeconds') {
-  const [startTime, endTime] = [start, end].map((time) => moment(time));
-  const duration = moment.duration(endTime.diff(startTime));
+function get_time_diff(start, end, fn_string = 'asSeconds') {
+    const [start_time, end_time] = [start, end].map((time) => moment(time));
+    const duration = moment.duration(end_time.diff(start_time));
 
-  return duration[fnString]();
+    return duration[fn_string]();
 }
 
-function formatSqlDt(dt) {
-  const dateTime = moment(dt);
+function format_sql_dt(dt) {
+    const date_time = moment(dt);
 
-  return dateTime.format('YYYY-MM-DD HH:mm:ss');
+    return date_time.format('YYYY-MM-DD HH:mm:ss');
 }
 
 module.exports = {
-  getBaseHour,
-  getTimeDiff,
-  formatSqlDt,
+    get_base_hour,
+    get_time_diff,
+    format_sql_dt,
 };
--- tests.js
+++ tests.js
@@ -1,27 +1,29 @@
+'use strict';
+
 const tap = require('tap');
 const {
-  getBaseHour,
-  getTimeDiff,
-  formatSqlDt,
+    get_base_hour,
+    get_time_diff,
+    format_sql_dt,
 } = require('./moment-utils');
 
 tap.test('Get base hour', async (t) => {
-  const dt = '2022-01-01 10:01:01';
-  const baseHour = getBaseHour(dt).format('HH:mm:ss').toString();
-  t.equal(baseHour, '10:00:00');
+    const dt = '2022-01-01 10:01:01';
+    const base_hour = get_base_hour(dt).format('HH:mm:ss').toString();
+    t.equal(base_hour, '10:00:00');
 });
 
 tap.test('Get time difference', async (t) => {
-  const start = '2022-01-01 10:00:00';
-  const end = '2022-01-01 10:01:00';
-  const timeDiffSeconds = getTimeDiff(start, end);
-  const timeDiffMinutes = getTimeDiff(start, end, 'asMinutes');
-  t.equal(timeDiffSeconds, 60);
-  t.equal(timeDiffMinutes, 1);
+    const start = '2022-01-01 10:00:00';
+    const end = '2022-01-01 10:01:00';
+    const time_diff_seconds = get_time_diff(start, end);
+    const time_diff_minutes = get_time_diff(start, end, 'asMinutes');
+    t.equal(time_diff_seconds, 60);
+    t.equal(time_diff_minutes, 1);
 });
 
 tap.test('Format SQL datetime', async (t) => {
-  const dt = '2022-01-01';
-  const formattedDt = formatSqlDt(dt).toString();
-  t.equal(formattedDt, '2022-01-01 00:00:00');
+    const dt = '2022-01-01';
+    const formatted_dt = format_sql_dt(dt).toString();
+    t.equal(formatted_dt, '2022-01-01 00:00:00');
 });
